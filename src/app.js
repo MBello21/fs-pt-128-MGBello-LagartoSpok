a661078b3a6c04860872b8f5c8becb0a69c60edd
MBello21 <magarbello@gmail.com>
2025-11-21 18:41:10 +0000

    proceso de interaccion con html y js

--- src/app.js
+++ src/app.js
@@ -17,12 +17,22 @@ const spinSelection = ((userSelection) => {
         lizard: ["spock", "paper"],
         spock: ["scissors", "rock"]
     }
+    let mensaje = ''
     let computerSelection = Math.floor(Math.random() * choises.length)
-    if (userSelection === Object.keys(rules)[computerSelection]) console.log('Empate');
-    else if (rules[choises[computerSelection]].includes(userSelection)) console.log('Ganaste');
-    else console.log('perdiste');
-
+    if (userSelection === Object.keys(rules)[computerSelection]) mensaje = 'Empate'
+    else if (rules[choises[computerSelection]].includes(userSelection)) mensaje = ' 🎊 Ganaste';
+    else mensaje ='perdiste';
+    document.getElementById('computerSelection').innerText = 'The computer select: ' + Object.keys(rules)[computerSelection];
+    document.getElementById('result').innerText = mensaje;
 
 })
 
-spinSelection('lizard')
+const buttons = document.querySelectorAll('#lizardSpock button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        
+        const valorJugador = event.currentTarget.value;
+        spinSelection(valorJugador);
+    });
+});
