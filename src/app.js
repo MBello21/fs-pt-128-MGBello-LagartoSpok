84b61c56e1a88e565d11eca5e9a392fe6ab97912
MBello21 <magarbello@gmail.com>
2025-11-21 11:26:13 +0000

    Lizard-Spock, con dos soluciones

--- src/app.js
+++ src/app.js
@@ -9,3 +9,27 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+// Codigo para ejercicio, Lagarto-spock
+
+// const spinSelection = ((userSelection) => {
+//     let computerSelection = Math.floor(Math.random() * choises.length)
+//     if (userSelection === Object.key(rules)[computerSelection]) console.log('Empate');
+//     else if (userSelection === Object.values(rules)[computerSelection][0] || userSelection === Object.values(rules)[computerSelection][1]) console.log('Perdiste');
+//     else if (userSelection !== Object.values(rules)[computerSelection][0] || userSelection !== Object.values(rules)[computerSelection][1]) console.log('ganaste');
+// })
+
+const spinSelection = ((userSelection) => {
+    const choises = ["rock", "paper", "scissors", "lizard", "spock"]
+    const rules = {
+        rock: ['scissors', "lizard"],
+        paper: ["rock", "spock"],
+        scissors: ["paper", "lizard"],
+        lizard: ["spock", "paper"],
+        spock: ["scissors", "rock"]
+    }
+    if(userSelection === Object.key(rules)[computerSelection]) console.log('Empate')
+    else if(rules[choises[computerSelection]].includes(userSelection))console.log('Ganaste');
+    else console.log('Perdiste');   
+})
+
+spinSelection('lizard')
