021ffb085eee0065df62e849e88f3afda21253ad
MBello21 <magarbello@gmail.com>
2025-11-22 11:39:55 +0000

    Subida de codigo comentado en js con actualizacion de fuegos artificiales, css copiado

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 
-// Codigo para ejercicio, Lagarto-spock
+// Codigo para ejercicio, Lagarto-spock primer codigo antes de intentar la solucion con includes()
 
 // const spinSelection = ((userSelection) => {
 //     let computerSelection = Math.floor(Math.random() * choises.length)
@@ -7,7 +7,7 @@
 //     else if (userSelection === Object.values(rules)[computerSelection][0] || userSelection === Object.values(rules)[computerSelection][1]) console.log('Perdiste');
 //     else if (userSelection !== Object.values(rules)[computerSelection][0] || userSelection !== Object.values(rules)[computerSelection][1]) console.log('ganaste');
 // })
-
+// Funcion para obtener eleccion del ordenador y comparar con la elegida con el usuario
 const spinSelection = ((userSelection) => {
     const choises = ["rock", "paper", "scissors", "lizard", "spock"]
     const rules = {
@@ -23,21 +23,26 @@ const spinSelection = ((userSelection) => {
     else if (rules[choises[computerSelection]].includes(userSelection)) mensaje = 'You Lose.';
     else mensaje = 'You win.';
 
-
+    //Inserción de texto mediante ID's para ver que eligio el ordenador y si hemos ganado, empatado o perdido
     document.getElementById('results').innerText = 'The result is: ';
     document.getElementById('computerSelection').innerText = 'The computer select: ' + Object.keys(rules)[computerSelection];
     document.getElementById('result').innerText = mensaje;
 
+    //Aunque el codigo de los Fuegos artificiales a sido copiado (HTML y CSS) aañadimos esta funcion para que solo se muestre 
+    //si se gana, creamos una constante que selecione todos los componentes con el id firework y la clase firework, para remover o 
+    //añadir o quitar el display none, como son varios fuegos artificiales usamos un forEach para recorrer todos los div con la clase
+    //firework y que quite a cada fuego artificial el d-none
     const fireworks = document.querySelectorAll('#firework .firework');
     if (mensaje === 'You win.') {
-        fireworks.forEach(fw => fw.classList.remove('d-none'));
-    }else{
-        fireworks.forEach(fw => fw.classList.add('d-none'));
+        fireworks.forEach(fire => fire.classList.remove('d-none'));
+    } else {
+        fireworks.forEach(fire => fire.classList.add('d-none'));
     }
 })
 
+//Hacemos algo similar a lo anterior pero con distintos values en HTML, para obtener la eleccion por botones del usuario
 const buttons = document.querySelectorAll('#lizardSpock button');
-
+//Con forEach recorremos cada boton para poder añadirle el addEventListener-> click para escuchar o ver el evento del click en el boton
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
 
